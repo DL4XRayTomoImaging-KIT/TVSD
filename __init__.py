56b9eb3387ad48c954cbd59c21981a172bd7da04
Yaroslav Zharov <mart.slaaf@gmail.com>
2020-12-22 17:38:08 +0100

    [+] requirements and __init__ added

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .TVSD import VolumeSlicingDataset, OneVolume, ExpandedPaddedSegmentation, ConvertableBoundingBoxes
+
+__all__ = ['VolumeSlicingDataset', 'OneVolume', 'ExpandedPaddedSegmentation', 'ConvertableBoundingBoxes']
